bce135bf33656d4c2dd83a950f2db9e953f9e0ba
Josef Šimánek <josef.simanek@gmail.com>
2022-12-02 23:20:34 +0100

    Another refactoring and adding weekend shows.

--- server/show.rb
+++ server/show.rb
@@ -1,31 +1,33 @@
 require 'open-uri'
 require 'nokogiri'
 
-class GoodMorning
+# TODO: remove, fixed in ruby 3.2
+# https://github.com/ruby/ruby/commit/fa2e1b67e548cb5653b66909a2bc3d6b9eae98e3
+Net::HTTPTemporaryRedirect = Net::HTTPPermanentRedirect
 
+class Show
   TV = 'https://www.ceskatelevize.cz'
   TV_TODAY = TV + '/tv-program/'
 
-  def today
-    find_topics rescue '' # freestyle
+  def today(show_title)
+    find_topics(show_title) #rescue false # freestyle
   end
 
   private
 
-  def find_topics
+  def find_topics(show_title)
     date = Date.today.strftime('%d.%m.%Y')
     tv_today = URI.open(TV_TODAY + date + '/').read
     tv_today_html = Nokogiri::HTML(tv_today)
 
-    link = tv_today_html.css('a').find {|el| el.text == 'Dobré ráno'}
-    return '' if link.nil?
+    link = tv_today_html.css('a').find {|el| el.text.match?(show_title)}
+    return false if link.nil?
 
     today = URI.open(TV + link.attributes['href'].value).read
     today_html = Nokogiri::HTML(today)
     description_el = today_html.css('div[class^=description] p').first
-    return '' if description_el.nil?
+    return false if description_el.nil?
 
     description_el.text
   end
 end
-
